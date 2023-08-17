5630a76bff7835fe6759a4b4043f7cbe59ff11a9
Lee hyunkyoung <hyunk.office@gmail.com>
2023-08-17 11:55:32 +0900

    init

---
+++ src/main/java/com/sparta/and/entity/User.java
@@ -0,0 +1,23 @@
+package com.sparta.and.entity;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String userPassword;
+    private String userName;
+    private String userEmail;
+//   private enum role;
+
+}
---
+++ src/test/java/com/sparta/and/AndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.and;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
