daa1bf6f27c675a8d300e3e69fab576a8b6906ea
Lim Suyoung <leepari20@gmail.com>
2023-09-12 16:43:10 +0900

    :sparkles: 공모전 검색 기능

--- src/main/java/com/sparta/and/service/ContestService.java
+++ src/main/java/com/sparta/and/service/ContestService.java
@@ -1,15 +1,14 @@
 package com.sparta.and.service;
 
-import java.util.List;
-
+import com.sparta.and.dto.SearchRequestDto;
 import com.sparta.and.dto.request.ContestRequestDto;
+import com.sparta.and.dto.response.ContestResponseDto;
 import com.sparta.and.entity.Contest;
+import com.sparta.and.repository.ContestRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import com.sparta.and.dto.response.ContestResponseDto;
-import com.sparta.and.repository.ContestRepository;
-
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -21,12 +20,21 @@ public class ContestService {
 		return contestRepository.findAllByOrderByCreatedDateDesc().stream().map(ContestResponseDto::new).toList();
 	}
 
-	public ContestResponseDto createContest (ContestRequestDto requestDto){
+	public ContestResponseDto createContest(ContestRequestDto requestDto) {
 		Contest createContest = new Contest(requestDto);
 		contestRepository.save(createContest);
 
 		return new ContestResponseDto(createContest);
 	}
 
+	public List<ContestResponseDto> searchContest(SearchRequestDto requestDto) {
+		List<ContestResponseDto> list = contestRepository.findByTitleContaining(
+				requestDto.getKeyword()).stream().map(ContestResponseDto::new).toList();
 
+		if (list.isEmpty()) {
+			throw new IllegalArgumentException("해당 검색결과가 없습니다.");
+		}
+
+		return list;
+	}
 }
