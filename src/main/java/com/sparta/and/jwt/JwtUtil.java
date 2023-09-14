31d68326c04b68f618aca19627b21c46f9f133ec
Lim Suyoung <leepari20@gmail.com>
2023-09-15 03:28:59 +0900

    :art: 배포 오류 개선중

--- src/main/java/com/sparta/and/jwt/JwtUtil.java
+++ src/main/java/com/sparta/and/jwt/JwtUtil.java
@@ -33,7 +33,7 @@ public class JwtUtil {
 	public static final String BEARER_PREFIX = "Bearer ";
 	private final long TOKEN_TIME = 60 * 60 * 1000L; // 60분
 
-	@Value("${spring.jwt.secret.key}")
+	@Value("${jwt.secret.key}")
 	private String secretKey;
 
 	private Key key;
