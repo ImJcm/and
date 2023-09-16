c59ccf7207aba69511a5938092973ebf9b6e44ec
Lim Suyoung <leepari20@gmail.com>
2023-09-16 13:59:47 +0900

    :art:

--- src/main/java/com/sparta/and/repository/ReportCommentRepository.java
+++ src/main/java/com/sparta/and/repository/ReportCommentRepository.java
@@ -5,9 +5,7 @@ import java.util.Optional;
 import com.sparta.and.entity.Comment;
 import com.sparta.and.entity.ReportComment;
 import com.sparta.and.entity.User;
-
-
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReportCommentRepository extends JpaRepository<ReportComment, Long> {
 	Optional<ReportComment> findByUserAndComment(User connectUser, Comment comment);
